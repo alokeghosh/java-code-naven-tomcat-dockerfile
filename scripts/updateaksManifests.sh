c98b11e602f6a3029413d5ae56e88e4f0958ef5a
Aloke <47846743+alokeghosh@users.noreply.github.com>
2025-10-11 08:43:07 +0530

    Update updateaksManifests.sh

--- scripts/updateaksManifests.sh
+++ scripts/updateaksManifests.sh
@@ -9,7 +9,7 @@ cd /tmp/temp_repo
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
 #sed -i "s|image:.*|image: <ACR-REGISTRY-NAME>/$2:$3|g" k8s-specifications/$1-deployment.yaml
-sed -i "s|image:.*|image: agpocacr.azurecr.io/$2:$3|g" aks_all_yaml/java-maven-deployment.yaml
+sed -i "s|image:.*|image: agpocacr.azurecr.io/:$3|g" aks_all_yaml/java-maven-deployment.yaml
 # Add the modified files
 git add .
 # Commit the changes
